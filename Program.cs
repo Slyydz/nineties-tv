194e5e34cd2be70e13d1604b746b4fa464f25088
Brady Williams <williams.cole29@yahoo.com>
2021-12-03 11:51:13 -0600

    Finished 18,19,20

--- Program.cs
+++ Program.cs
@@ -162,20 +162,28 @@ namespace NinetiesTV
         // 18. Return all dramas except for the highest rated.
         static List<Show> AllButBestDrama(List<Show> shows)
         {
-            throw new NotImplementedException();
+            return shows
+            .OrderByDescending(s => s.ImdbRating)
+            .Where(s => s.Genres.Contains("Drama"))
+            .Skip(1).ToList();
         }
 
         // 19. Return the number of crime shows with an IMDB rating greater than 7.0.
         static int GoodCrimeShows(List<Show> shows)
         {
-            throw new NotImplementedException();
+            return shows
+            .Where(s => s.ImdbRating > 7.0)
+            .Count();
         }
 
         // 20. Return the first show that ran for more than 10 years 
         //     with an IMDB rating of less than 8.0 ordered alphabetically.
         static Show FirstLongRunningTopRated(List<Show> shows)
         {
-            throw new NotImplementedException();
+            return shows
+            .OrderBy(s => s.Name)
+            .Where(s => s.ImdbRating < 8.0 && (s.EndYear - s.StartYear > 10))
+            .FirstOrDefault();
         }
 
         // 21. Return the show with the most words in the name.
